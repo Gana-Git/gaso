4b6f1cea3986fe77ce7a94492b77ebad0bd12ee7
Gana-Git <78214435+Gana-Git@users.noreply.github.com>
2025-04-27 01:06:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function afficherMessage() {
 
     // Vérification des champs
     if (prenom && nom) {
-        // Message personnalisé avec la blague
+        // Message personnalisé après avoir appuyé sur "Envoyer"
         document.getElementById("message").innerHTML = "Félicitations " + prenom + " " + nom + "! Tu as trouvé le bouton d'envoi, maintenant tu mérites une blague : Pourquoi le serveur a-t-il traversé la route ? Parce qu'il voulait éviter un 404. 😄";
     } else {
         document.getElementById("message").innerHTML = "Oups! Tu as oublié ton prénom ou ton nom. 😅";
